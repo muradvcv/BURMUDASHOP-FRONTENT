e3841c38609951069c5c9e09a746f66299a049b1
muradvcv <muradvcv@gmail.com>
2026-07-11 13:49:07 +0600

    frist commit

---
+++ src/Components/Ts/practice.ts
@@ -0,0 +1,19 @@
+const me: {
+  name: string,
+  age: number,
+  address: string
+  isMarried: boolean
+} = {
+  name: "BurmudaTech",
+  age: 1,
+  address: "Seoul",
+  isMarried: true
+}
+
+function adder (a: number, b: number):number{
+  return a + b
+}
+adder(1, 2)
+
+// 
+let id :string | number
